5814322194795ec1cae681530868ca449fda199a
Afzal Ali <52553046+afzalali27@users.noreply.github.com>
2020-09-07 11:12:17 +0500

    index page
    complete design of index page and addes two images for index page in bg-img folder

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,8 +9,7 @@ namespace VideoMag
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/js/jquery/jquery-2.2.4.min.js"
+                        "~/Scripts/jquery-{version}.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
